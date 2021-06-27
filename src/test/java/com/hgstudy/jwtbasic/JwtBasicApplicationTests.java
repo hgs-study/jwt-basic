f5aabc269fbc519be1908ecd3161c46db4f6d7c7
hgs-study <hgstudy_@naver.com>
2021-06-27 20:31:46 +0900

    jwt basic 프로젝트 생성

---
+++ src/test/java/com/hgstudy/jwtbasic/JwtBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgstudy.jwtbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
