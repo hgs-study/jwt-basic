4f589f0bb8eb1b27bd801668bd8910b35572d051
hgs-study <hgstudy_@naver.com>
2021-07-01 22:46:33 +0900

    브랜치 생성

--- src/main/java/com/hgstudy/jwtbasic/jwt/JwtProperties.java
+++ src/main/java/com/hgstudy/jwtbasic/jwt/JwtProperties.java
@@ -8,5 +8,5 @@ public class JwtProperties {
     public static final String ACCESS_TOKEN = "Access_token";
     public static final String REFRESH_TOKEN = "Authorization";
     public static final long ACCESS_TOKEN_EXPIRATION_TIME = 2 * 60 * 1000L; // 2분
-    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 1 * 60 * 60 * 1000L; // 하루
+    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 1 * 60 * 60 * 1000L; // 하루d
 }
